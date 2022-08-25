c23d23eac79880d8f40e669fa2bcdd598e1d7d3a
wnbo9 <94724702+wnbo9@users.noreply.github.com>
2022-08-25 14:52:58 -0400

    Update ImageRegistration.R

--- ImageRegistration.R
+++ ImageRegistration.R
@@ -13,10 +13,8 @@ colnames(dat) = c("geneID", "y", "x", "MIDCounts")
 summary(dat$x)
 summary(dat$y)
 
-p1 = dat[1:100000,] %>% ggplot(aes(x=x, y=y)) + 
+dat[1:100000,] %>% ggplot(aes(x=x, y=y)) + 
   geom_point(size=0.02) + 
   coord_fixed(ratio = 1) +
   geom_vline(xintercept = c(0, 17000)) +
   geom_hline(yintercept = c(0, 24000)) + NoLegend()
-
-ggsave(p1, "Barcode.pdf", dpi = 300, width = 16, height = 24, units = "cm")
